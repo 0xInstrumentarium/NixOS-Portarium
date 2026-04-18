c03cac21f9d1ab1a1b0eb28090833b56ebc532a0
n3023723 <jamesblaser@gmx.net>
2026-04-18 17:45:16 +0200

    default

--- configuration.nix
+++ configuration.nix
@@ -7,21 +7,28 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-72778297-4173-42da-bd7e-7930879ed417".device = "/dev/disk/by-uuid/72778297-4173-42da-bd7e-7930879ed417";
-  networking.hostName = "portarium";
+  boot.initrd.luks.devices."luks-56bf288b-8718-4249-a686-091d9ac6fcda".device = "/dev/disk/by-uuid/56bf288b-8718-4249-a686-091d9ac6fcda";
+  networking.hostName = "portarium"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Set your time zone.
   time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -43,13 +50,16 @@
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "no";
     variant = "";
   };
-  console.keyMap = "no";
+
+  # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -57,6 +67,11 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
 
@@ -68,51 +83,52 @@
     isNormalUser = true;
     description = "james";
     extraGroups = [ "networkmanager" "wheel" ];
-    
     packages = with pkgs; [
+    #  thunderbird
     ];
   };
-  
-  services.openssh ={
-  enable = true;
-  settings={
-	X11Forwarding = true;
-	PermitRootLogin = "no";
-	PasswordAuthentication = true;
-	}; 
-  openFirewall = true;
- };
+
   # Install firefox.
   programs.firefox.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  
-  # Enable the Flakes feature and the accompanying new nix command-line tool
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Flakes clones its dependencies through the git command,
-  # so git must be installed first
   environment.systemPackages = with pkgs; [
-    git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    niri
-    wget
-    ghostty
-    tealdeer
-    xclip
-    bat
-    home-manager
-  
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
-  environment.variables.EDITOR = "vim";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
+
+
+
