cb506dfbdef3dde3a2592d2c67a7145e057e7b51
n3023723 <jamesblaser@gmx.net>
2026-04-18 18:07:34 +0200

    testing

--- configuration.nix
+++ configuration.nix
@@ -77,9 +77,13 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
+  programs.zsh ={
+    enable = true;
+    
+  }
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.james = {
+    shell = pkgs.zsh;
     isNormalUser = true;
     description = "james";
     extraGroups = [ "networkmanager" "wheel" ];
