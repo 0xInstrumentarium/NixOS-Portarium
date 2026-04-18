2d2250150d56f375c0d5e96eb2946c4dcf68046d
n3023723 <jamesblaser@gmx.net>
2026-04-18 17:52:29 +0200

    testing

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,10 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  vim
+  git
+  wget
+
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -129,6 +133,5 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
-
 
 
